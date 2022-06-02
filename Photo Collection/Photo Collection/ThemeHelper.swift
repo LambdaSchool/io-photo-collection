2aa0b2bd6eb8ae06ff292727508c4ea998d8859d
Waseem Idelbi <idelbiw@gmail.com>
2022-06-01 20:39:01 -0400

    created ThemeHelper

---
+++ Photo Collection/Photo Collection/ThemeHelper.swift
@@ -0,0 +1,38 @@
+//
+//  ThemeHelper.swift
+//  Photo Collection
+//
+//  Created by Waseem Idelbi on 6/1/22.
+//
+
+import Foundation
+
+class ThemeHelper {
+    
+//MARK: - Properties
+    // Theme key for User Defaults
+    let themePreferenceKey = "ThemeKey"
+    // Last theme preference set
+    var themePreference: String? {
+        return UserDefaults.value(forKey: themePreferenceKey) as? String
+    }
+    
+//MARK: - Methods
+    // Set dark theme
+    func setThemePreferenceToDark() {
+        UserDefaults.standard.set("Dark", forKey: themePreferenceKey)
+    }
+    // Set green theme
+    func setThemePreferenceToGreen() {
+        UserDefaults.standard.set("Green", forKey: themePreferenceKey)
+    }
+    
+    // Init method
+    init() {
+        if themePreference == nil {
+            setThemePreferenceToDark()
+        }
+    }
+    
+    
+}
