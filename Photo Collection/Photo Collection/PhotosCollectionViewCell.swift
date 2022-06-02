bda851ccb91e0a7707002bdfbfdb2d33de085c9b
Waseem Idelbi <idelbiw@gmail.com>
2022-06-01 21:22:51 -0400

    Added some logic in the collection view cell to handle updating views

--- Photo Collection/Photo Collection/PhotosCollectionViewCell.swift
+++ Photo Collection/Photo Collection/PhotosCollectionViewCell.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class PhotosCollectionViewCell: UICollectionViewCell {
     
+//MARK: - Properties
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+//MARK: - Methods
+    // Updates the views
+    func updateViews() {
+        guard let photo = photo else { return }
+        titleLabel.text = photo.title
+        imageView.image = UIImage(data: photo.imageData)
+    }
+    
 //MARK: - IBOutlets
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
