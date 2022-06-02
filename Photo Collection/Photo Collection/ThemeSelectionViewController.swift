34963e923131b1962b784000413385a514476547
Waseem Idelbi <idelbiw@gmail.com>
2022-06-01 21:19:31 -0400

    Added some UI for theme selection, created a class file for the new view controller and wired them up together

---
+++ Photo Collection/Photo Collection/ThemeSelectionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThemeSelectionViewController.swift
+//  Photo Collection
+//
+//  Created by Waseem Idelbi on 6/1/22.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+//MARK: - Properties
+    var themeHelper: ThemeHelper?
+    
+//MARK: - IBActions
+    // Dark theme button is tapped
+    @IBAction func selectDarkTheme(_ sender: UIButton) {
+    }
+    // Green theme button is tapped
+    @IBAction func selectGreenTheme(_ sender: UIButton) {
+    }
+    
+
+}
