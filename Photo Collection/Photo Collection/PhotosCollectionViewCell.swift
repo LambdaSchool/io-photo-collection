f19c53518bc64c6ae6f0cace5c83cdfdce1ce301
Waseem Idelbi <idelbiw@gmail.com>
2022-06-01 20:52:16 -0400

    Added some UI in the storyboard and wired it up with new collectionViewController and CollectionViewCell files

---
+++ Photo Collection/Photo Collection/PhotosCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photo Collection
+//
+//  Created by Waseem Idelbi on 6/1/22.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+//MARK: - IBOutlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+}
